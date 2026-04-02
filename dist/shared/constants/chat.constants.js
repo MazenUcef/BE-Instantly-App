039e66322e5099960223ab39aea8d72955264d82
MazenAfifi <mazen.afifi@flairstech.com>
2026-04-02 11:55:00 +0200

    fixes

--- dist/shared/constants/chat.constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CHAT_SOCKET_EVENTS = exports.CHAT_SESSION_BLOCKED_STATUSES = void 0;
-exports.CHAT_SESSION_BLOCKED_STATUSES = ["completed", "cancelled"];
-exports.CHAT_SOCKET_EVENTS = {
-    MESSAGE_NEW: "message:new",
-    MESSAGE_READ: "message:read",
-    CHAT_SYNC: "chat:sync",
-};
